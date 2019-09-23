d0cea299a63bf24865597d830e56994af4140113
Kyle Edric Aranas <kebaranas@gmail.com>
2019-09-23 14:41:24 +0800

    Fixed some of the errors

--- globeconnect/tests/subscriber.js
+++
@@ -1,6 +0,0 @@
-globe = require('../index.js');
-subscriber = globe.Subscriber('[token]');
-subscriber.setAddres('[address]');
-subscriber.getSubscriberBalance(function(resCode, body) {
-    console.log(body);
-});
